790f270bd9f7d97715753aec6f31ba52289d6593
Felipe Dutra <felipemd.dutra@gmail.com>
2025-04-05 11:15:58 -0300

    add simple GET route to router, successfully working :)

--- tests/server_test.c
+++ tests/server_test.c
@@ -1,7 +1,18 @@
 #include <cgin/server.h>
+#include <stdio.h>
+
+#include "cgin/declarations.h"
+
+int home_handler(client_t* c) {
+    (void)c;
+    printf("Welcome home!\n");
+    return 0;
+}
 
 int main() {
     router_t* r = new_router(8080);
+
+    r->GET(r, "/home", home_handler);
 
     router_run(r);
     free_router(r);
