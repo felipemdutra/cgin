87740cd1db57e4309259947fb61af01d6dd5278b
Felipe Dutra <felipemd.dutra@gmail.com>
2025-04-05 00:57:25 -0300

    declare core functions Declared routeGET functions that adds a new GET route to the given router Declared read_req function that reads the request from a client socket Declared handle_req function that matches the information given and calls that route's handler function Added comments above every function explaining their purpose and what do they do

--- include/cgin/route.h
+++ include/cgin/route.h
@@ -1,13 +1,33 @@
 #ifndef __ROUTE_H__
 #define __ROUTE_H__
 
-typedef int (*route_handler_fn)(int client_sock);
+#include "cgin/declarations.h"
 
-typedef struct {
-    const char *method;       // e.g. GET, POST, etc...
-    const char *path;         // e.g. "/home/user/me"
-    route_handler_fn handler;
-} route_t;
+/**
+ * Adds a new GET route to @param r->routes.
+ *
+ * @param r The router to add the route to.
+ * @param path The route's path.
+ * @param route_handler_fn The route's handler function.
+ */
+void routeGET(router_t *r, char *path, route_handler_fn);
+
+/**
+ * Parses client requests using client's file descriptor and returns the
+ * requested path.
+ *
+ * @param clientsock The client's socket.
+ * @return The requested path, which can then be used to compare to router_t's
+ * routes path's, or NULL on failure.
+ */
+struct ReqInfo* read_req(int clientsock);
+
+/**
+ * This function compares the information given in @param info to all routes
+ * associated with @param r. If no route matches, 404 that bitch, else we call
+ * the matching route's handler function.
+ */
+int handle_req(router_t *r, struct ReqInfo* info);
 
 #endif
 
